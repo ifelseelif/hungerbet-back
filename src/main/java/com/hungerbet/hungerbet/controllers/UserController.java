5d92c6bd74b31f511464de2c6cd0cad05c9c167e
ifelseelif <264451@niuitmo.ru>
2023-10-04 12:47:44 +0300

    added game initializer

--- src/main/java/com/hungerbet/hungerbet/controllers/UserController.java
+++ src/main/java/com/hungerbet/hungerbet/controllers/UserController.java
@@ -1,11 +1,14 @@
 package com.hungerbet.hungerbet.controllers;
 
+import com.hungerbet.hungerbet.entity.domain.User;
+import com.hungerbet.hungerbet.entity.exceptions.NotFoundException;
+import com.hungerbet.hungerbet.repository.UserRepository;
 import com.hungerbet.hungerbet.service.implementaion.UserInfoService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @SecurityRequirement(name = "bearerAuth")
@@ -15,8 +18,19 @@ public class UserController
     @Autowired
     private UserInfoService service;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/is-auth")
     public String IsAuthorized() {
         return "Authorized";
+    }
+
+    @GetMapping
+    public User getUser(@RequestParam String login) throws NotFoundException {
+        User user =  userRepository.findByLogin(login).orElseThrow(() -> new NotFoundException("Not found"));
+        user.getManager().setUser(null);
+        user.getManager().setGame(null);
+        return user;
     }
 }
