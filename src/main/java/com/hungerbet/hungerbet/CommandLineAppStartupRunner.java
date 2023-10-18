e11ab53e4e30b2aa31738fe762b52f89179e62f4
ifelseelif <264451@niuitmo.ru>
2023-10-18 17:33:59 +0300

    more data for init

--- src/main/java/com/hungerbet/hungerbet/CommandLineAppStartupRunner.java
+++ src/main/java/com/hungerbet/hungerbet/CommandLineAppStartupRunner.java
@@ -1,10 +1,8 @@
 package com.hungerbet.hungerbet;
 
-import com.hungerbet.hungerbet.entity.domain.Game;
-import com.hungerbet.hungerbet.entity.domain.GameStatus;
-import com.hungerbet.hungerbet.entity.domain.Player;
-import com.hungerbet.hungerbet.entity.domain.User;
+import com.hungerbet.hungerbet.entity.domain.*;
 import com.hungerbet.hungerbet.repository.GameRepository;
+import com.hungerbet.hungerbet.repository.PlannedEventRepository;
 import com.hungerbet.hungerbet.repository.PlayerRepository;
 import com.hungerbet.hungerbet.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +10,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Component
@@ -27,6 +27,9 @@ public class CommandLineAppStartupRunner implements CommandLineRunner {
 
     @Autowired
     private PlayerRepository participantRepository;
+
+    @Autowired
+    private PlannedEventRepository plannedEventRepository;
 
     @Autowired
     private PasswordEncoder encoder;
@@ -47,59 +50,98 @@ public class CommandLineAppStartupRunner implements CommandLineRunner {
             return adminUser;
         });
 
-        int randomNum = ThreadLocalRandom.current().nextInt(1, 100 + 1);
-        Player player = new Player("Artur Clone " + randomNum, "Kuprtianov", 23, "male");
-        int randomNum2 = ThreadLocalRandom.current().nextInt(1, 100 + 1);
-        Player player2 = new Player("Artur Clone " + randomNum2, "Kuprtianov", 23, "male");
-        participantRepository.save(player);
-        participantRepository.save(player2);
-
         if (gameRepository.findByName("Типа draft Голодные игры #75").isEmpty()) {
-            Game game = new Game("Голодные игры DRAFT",
+            Game game = new Game("Типа draft Голодные игры #75",
+                    GameStatus.DRAFT,
+                    new Date(2145, Calendar.JULY, 23),
+                    "Состоит из 12 секторов, в каждом из которых по очереди активируется определенное опасное явление. Рог Изобилия находится посередине и представляет собой остров, окруженный соленой водой.",
+                    "",
+                    "",
+                    admin);
+
+            gameRepository.save(game);
+        }
+
+        if (gameRepository.findByName("Голодные игры #75 ready for PLANNED").isEmpty()) {
+            Game game = new Game("Голодные игры #75 ready for PLANNED",
                     GameStatus.DRAFT,
                     new Date(2145, Calendar.JULY, 23),
                     "Состоит из 12 секторов, в каждом из которых по очереди активируется определенное опасное явление. Рог Изобилия находится посередине и представляет собой остров, окруженный соленой водой.",
                     "Квартальная бойня. Все участники являются победителями прошлых игр. Единственный источник воды - стволы деревьев, растущие в лесу.",
                     "Джунгли",
                     admin);
+
             gameRepository.save(game);
         }
 
-        if (gameRepository.findByName("Голодные игры #75").isEmpty()) {
-            Game game = new Game("Голодные игры #75",
+        if (gameRepository.findByName("Голодные игры #75 ready for ONGOING").isEmpty()) {
+            Game game = new Game("Голодные игры #75 ready for ONGOING",
                     GameStatus.PLANNED,
                     new Date(2145, Calendar.JULY, 23),
                     "Состоит из 12 секторов, в каждом из которых по очереди активируется определенное опасное явление. Рог Изобилия находится посередине и представляет собой остров, окруженный соленой водой.",
                     "Квартальная бойня. Все участники являются победителями прошлых игр. Единственный источник воды - стволы деревьев, растущие в лесу.",
                     "Джунгли",
                     admin);
+
+            List<Player> playerList = new ArrayList<>();
+            for (int i = 0; i < 12; i++) {
+                int randomNum = ThreadLocalRandom.current().nextInt(1, 100 + 1);
+                Player player = new Player("Artur Clone " + randomNum, "Kuprtianov", 23, "male");
+                participantRepository.save(player);
+                playerList.add(player);
+            }
+
+            for (int i = 0; i < 12; i++) {
+                int randomNum = ThreadLocalRandom.current().nextInt(1, 100 + 1);
+                Player player = new Player("Maria Clone " + randomNum, "Kuprtianova", 23, "female");
+                playerList.add(player);
+                participantRepository.save(player);
+            }
+
+
+            for (int i = 0; i < 24; i++) {
+                game.attachPlayer(playerList.get(i));
+            }
+
             gameRepository.save(game);
         }
 
-        if (gameRepository.findByName("Голодные игры #74").isEmpty()) {
-            Game game = new Game("Голодные игры #74",
+
+        if (gameRepository.findByName("Голодные игры #73 ONGOING").isEmpty()) {
+            Game game = new Game("Голодные игры #73 ONGOING",
                     GameStatus.ONGOING,
-                    new Date(2145, Calendar.JULY, 23),
-                    "Состоит из 12 секторов, в каждом из которых по очереди активируется определенное опасное явление. Рог Изобилия находится посередине и представляет собой остров, окруженный соленой водой.",
-                    "Квартальная бойня. Все участники являются победителями прошлых игр. Единственный источник воды - стволы деревьев, растущие в лесу.",
-                    "Лес",
-                    admin);
-            game.attachPlayer(player);
-            gameRepository.save(game);
-        }
-
-        if (gameRepository.findByName("Голодные игры #73").isEmpty()) {
-            Game game = new Game("Голодные игры #73",
-                    GameStatus.COMPLETED,
                     new Date(2145, Calendar.JULY, 23),
                     "Состоит из 12 секторов, в каждом из которых по очереди активируется определенное опасное явление. Рог Изобилия находится посередине и представляет собой остров, окруженный соленой водой.",
                     "Квартальная бойня. Все участники являются победителями прошлых игр. Единственный источник воды - стволы деревьев, растущие в лесу.",
                     "Разрушенный город",
                     admin);
-            game.setWinner(player);
-            game.attachPlayer(player);
-            game.attachPlayer(player2);
+
+            List<Player> playerList = new ArrayList<>();
+            for (int i = 0; i < 12; i++) {
+                int randomNum = ThreadLocalRandom.current().nextInt(1, 100 + 1);
+                Player player = new Player("Artur Clone " + randomNum, "Kuprtianov", 23, "male");
+                participantRepository.save(player);
+                playerList.add(player);
+            }
+
+            for (int i = 0; i < 12; i++) {
+                int randomNum = ThreadLocalRandom.current().nextInt(1, 100 + 1);
+                Player player = new Player("Maria Clone " + randomNum, "Kuprtianova", 23, "female");
+                participantRepository.save(player);
+                playerList.add(player);
+            }
+
+            for (int i = 0; i < 24; i++) {
+                game.attachPlayer(playerList.get(i));
+            }
+
             gameRepository.save(game);
+            Calendar date = Calendar.getInstance();
+            long timeInSecs = date.getTimeInMillis();
+            Date afterAdding2Mins = new Date(timeInSecs + (2 * 60 * 1000));
+            Date afterAdding5Mins = new Date(timeInSecs + (5 * 60 * 1000));
+            plannedEventRepository.save(new PlannedEvent(game.getId(), "Армагедон", "Большой бум", afterAdding2Mins));
+            plannedEventRepository.save(new PlannedEvent(game.getId(), "Водопад", "Много воды", afterAdding5Mins));
         }
 
         if (gameRepository.findByName("Голодные игры #72").isEmpty()) {
@@ -110,8 +152,13 @@ public class CommandLineAppStartupRunner implements CommandLineRunner {
                     "Квартальная бойня. Все участники являются победителями прошлых игр. Единственный источник воды - стволы деревьев, растущие в лесу.",
                     "Остров",
                     admin);
-            game.setWinner(player);
+
+            int randomNum = ThreadLocalRandom.current().nextInt(1, 100 + 1);
+            Player player = new Player("Maria Clone " + randomNum, "Kuprtianova", 23, "female");
+            participantRepository.save(player);
+
             game.attachPlayer(player);
+            game.setWinner(player);
             gameRepository.save(game);
         }
     }
